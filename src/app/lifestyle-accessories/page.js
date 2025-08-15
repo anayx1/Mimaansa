4eb75c3a6f1a237a51663b64eac39755feff6800
anayx1 <anaytiwarii123@gmail.com>
2025-08-15 18:47:52 +0530

    apparel images update

---
+++ src/app/lifestyle-accessories/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ApparelHero from '../components/Apparel/ApparelHero'
+import ApparelImageSection from '../components/Apparel/ImageSection'
+import BuiltWithCare from '../components/Apparel/BuildWithCare'
+
+const page = () => {
+    return (
+        <>
+            <ApparelHero />
+            <ApparelImageSection />
+            <BuiltWithCare />
+
+        </>
+    )
+}
+
+export default page
