18b9cddf8614c4f18c6dbda755d65dee74ccbb30
anayx1 <anaytiwarii123@gmail.com>
2025-08-31 14:06:10 +0530

    footer

--- src/app/components/Homepage/Footer.js
+++ src/app/components/Homepage/Footer.js
@@ -30,7 +30,7 @@ export default function Footer() {
                         transition={{ duration: 0.8 }}
                         viewport={{ once: true }}
                     >
-                        <h2 className="text-3xl sm:text-4xl md:text-5xl font-normal text-white mb-8 leading-relaxed max-w-5xl mx-auto px-4">
+                        <h2 className="text-3xl md:text-5xl font-normal text-white mb-8 leading-relaxed max-w-6xl mx-auto px-4">
                             Are you ready to simplify your sourcing and
                             <br className="hidden md:block" />
                             manufacturing in India?
@@ -45,10 +45,10 @@ export default function Footer() {
                     {/* Info + Logo Section */}
                     <section className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-10 w-full mt-10">
                         {/* Contact Info */}
-                        <div className="flex flex-col items-center lg:items-start justify-center gap-1 text-center lg:text-left text-white">
-                            <p className="text-white font-medium mb-1  font-navbar">CONTACT INFO</p>
-                            <p className="text-white font-navbar">+91 9910924032</p>
-                            <p className="text-white font-navbar">info@mimaansa.com</p>
+                        <div className="flex flex-col items-center justify-center gap-0 text-center text-white">
+                            <span className="text-white font-medium font-navbar ">CONTACT INFO</span>
+                            <span className="text-white font-navbar text-base">+91 9910924032</span>
+                            <span className="text-white font-navbar text-base">info@mimaansa.com</span>
                         </div>
 
                         {/* Logo */}
@@ -64,7 +64,7 @@ export default function Footer() {
                                 width={1000}
                                 height={1000}
                                 alt="logo"
-                                className="w-[220px] sm:w-[280px] md:w-[350px] h-auto"
+                                className="w-[220px] md:w-[300px] h-auto"
                             />
                         </motion.div>
 
@@ -72,13 +72,13 @@ export default function Footer() {
                         <div className="flex flex-col items-center lg:items-end justify-center gap-1 text-center text-white">
                             <span className="w-auto flex flex-col space-y-2">
 
-                                <Link href="/terms-and-conditions" className="text-white hover:text-gray-300 transition-colors font-navbar relative group">
+                                <Link href="/terms-and-conditions" className="text-base text-white hover:text-gray-300 transition-colors font-navbar relative group">
                                     TERMS & CONDITIONS
                                     <span
                                         className="absolute left-0 -bottom-1 w-0 h-[2px] bg-gray-200 transition-all duration-400 group-hover:w-full"
                                     />
                                 </Link>
-                                <Link href="/privacy-policy" className="text-white hover:text-gray-300 transition-colors font-navbar relative group text-center">
+                                <Link href="/privacy-policy" className="text-base text-white hover:text-gray-300 transition-colors font-navbar relative group text-center">
                                     PRIVACY POLICY
                                     <span
                                         className="absolute left-0 -bottom-1 w-0 h-[2px] bg-gray-200 transition-all duration-400 group-hover:w-full"
@@ -90,7 +90,7 @@ export default function Footer() {
 
                     {/* Description */}
                     <motion.p
-                        className="text-white text-sm sm:text-base lg:text-lg leading-relaxed max-w-4xl mx-auto text-center mt-12 lg:mt-16 px-4"
+                        className="text-white text-base lg:text-md leading-relaxed max-w-5xl mx-auto text-center mt-12 lg:mt-16 px-4"
                     >
                         Let's start a conversation. Whether you're a growing brand or an established retailer, Mimaansa is here to
                         support your journey. Get in touch with us today to discuss your needs and discover how we can deliver the
@@ -99,10 +99,10 @@ export default function Footer() {
                 </div>
 
                 {/* Footer Bottom */}
-                <div className="pt-10">
+                <div className="pt-3">
                     {/* Social Links */}
                     <motion.div
-                        className="flex flex-wrap justify-center gap-5 items-center mb-8"
+                        className="flex flex-wrap justify-center gap-5 items-center mb-4"
 
                     >
 
@@ -140,7 +140,7 @@ export default function Footer() {
                     <motion.div
                         className="text-center">
                         <Link href={'https://vikavalabs.com/'} target="_blank">
-                            <p className="text-white text-xs sm:text-sm tracking-wider ">CRAFTED BY <em className="underline">VIKAVA LABS</em></p>
+                            <p className="text-white text-xs  tracking-wider ">CRAFTED BY <em className="underline">VIKAVA LABS</em></p>
                         </Link>
                     </motion.div>
                 </div>
